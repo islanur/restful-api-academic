4a21e3f40fe0464037740ee4ac2df7a5aa8a591b
islanur <islanur@gmail.com>
2023-10-31 23:47:16 +0800

    Add login method with get token

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AuthLoginRequest;
 use App\Http\Requests\AuthRegisterRequest;
 use App\Http\Resources\AuthResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -15,5 +17,24 @@ class AuthController extends Controller
 
         return (new AuthResource(true, 'Success register user', $user))
             ->response()->setStatusCode(201);
+    }
+
+    public function login(AuthLoginRequest $request): JsonResponse
+    {
+        $user = $request->validated();
+
+        if (!Auth::attempt(['email' => $user['email'], 'password' => $user['password']])) {
+            return (new AuthResource(false, 'Invalid Credentials', null))
+                ->response()->setStatusCode(401);
+        }
+
+        /** @var \App\Models\User $user **/
+        $user = Auth::user();
+        $token = $user->createToken('token')->plainTextToken;
+
+        Auth::login($user);
+
+        return (new AuthResource(true, 'Success login', $user, $token))
+            ->response()->setStatusCode(200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['namespace' => 'App\Http\Controllers'], function () {
     Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
 });
